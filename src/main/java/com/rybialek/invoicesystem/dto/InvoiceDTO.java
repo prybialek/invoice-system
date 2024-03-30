cf08d14229ef6a39a631ec1d0beb24ac42f82788
Pawel Rybialek <pawel.rybialek@gmail.com>
2024-03-31 00:01:28 +0100

    gui fix

--- src/main/java/com/rybialek/invoicesystem/dto/InvoiceDTO.java
+++ src/main/java/com/rybialek/invoicesystem/dto/InvoiceDTO.java
@@ -2,7 +2,6 @@ package com.rybialek.invoicesystem.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,16 +12,8 @@ public class InvoiceDTO {
     @NotBlank(message = "Name is required")
     @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
     private BigDecimal amount;
-
-    public InvoiceDTO(Long id, String name, LocalDate date, BigDecimal amount) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.amount = amount;
-    }
 
     public InvoiceDTO() {
     }
